671df6b6c8dacf890b83490327eededcd40903b2
Zemtsova Anna <zemtsova18anna@gmail.com>
2019-02-21 18:16:02 +0200

    SDf

---
+++ src/SFD.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class SFD {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextInt();
+    }
+}
